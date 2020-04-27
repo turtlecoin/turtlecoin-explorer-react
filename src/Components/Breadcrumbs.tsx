b8db609804aea5fd3022bb0a2f92b02c070991cf
ExtraHash <ExtraHash@protonmail.com>
2020-04-26 22:54:57 -0600

    more responsiveness changes

--- src/Components/Breadcrumbs.tsx
+++ src/Components/Breadcrumbs.tsx
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getWindowDimensions } from '../Utils/getWindowDimensions';
 
 type Props = {
   match: any;
 };
-type State = {};
+type State = {
+  windowDimensions: any;
+};
 
 export class Breadcrumbs extends Component<Props, State> {
   state: State;
   constructor(props: Props) {
     super(props);
-    this.state = {};
+    this.state = {
+      windowDimensions: getWindowDimensions(),
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      windowDimensions: getWindowDimensions(),
+    });
   }
 
   render() {
     const { match } = this.props;
+    const { windowDimensions } = this.state;
     const pathParts = match.url.split('/');
 
     if (pathParts[0] === '' && pathParts[1] === '') {
@@ -51,9 +72,12 @@ export class Breadcrumbs extends Component<Props, State> {
                       }
                     >
                       <Link to={`/${part}`} className="breadcrumb-link">
-                        {pathParts.indexOf(part) <= 1
-                          ? part.charAt(0).toUpperCase() + part.slice(1)
-                          : part}
+                        {pathParts.indexOf(part) <= 1 &&
+                          part.charAt(0).toUpperCase() + part.slice(1)}
+                        {pathParts.indexOf(part) > 1 &&
+                          (windowDimensions.width > 1023
+                            ? part
+                            : part.slice(0, 10))}
                       </Link>
                     </li>
                   );
